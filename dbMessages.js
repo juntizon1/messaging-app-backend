61f68f4e8fcfe47b275eae22a7a76217091432c0
juntizon1 <123132298+juntizon1@users.noreply.github.com>
2023-05-03 20:31:11 -0600

    Update dbMessages.js
    added reactions

--- dbMessages.js
+++ dbMessages.js
@@ -3,6 +3,7 @@ const messagingSchema = mongoose.Schema({
     message:String,
     name:String,
     timestamp:String,
-    received:Boolean
+    received:Boolean,
+    reactions:[({type:String})]
 })
 export default mongoose.model('messages',messagingSchema)
